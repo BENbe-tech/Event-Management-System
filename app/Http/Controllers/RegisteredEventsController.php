df9f712d2176da63ccb3aa281df6fc8effd9abb3
Bernard Augustine <kayombobernard@gmail.com>
2022-06-15 10:27:45 +0300

    edited controller api

--- app/Http/Controllers/RegisteredEventsController.php
+++ app/Http/Controllers/RegisteredEventsController.php
@@ -141,19 +141,19 @@ class RegisteredEventsController extends Controller
 
 
 
-       echo "success";
-        // return response()->json(['success'=>'You have registered successfuly to this event']);
+    //    echo "success";
+        return response()->json(['success'=>'You have registered successfuly to this event']);
         }
     }
     else{
-        // return response()->json(['success'=>'The event has arleady ended']);
-   echo "already";
+        return response()->json(['success'=>'The event has arleady ended']);
+//    echo "already";
     }
     }
     else{
-echo "failed";
+// echo "failed";
 
-        // return response()->json(['success'=>'Failed to register, login first']);
+        return response()->json(['success'=>'Failed to register, login first']);
     }
     }
 
