e5d9878fa6a7cb6ac242027bec329a7132b90984
Bernard Augustine <kayombobernard@gmail.com>
2022-06-22 00:19:50 +0300

    report completed 3

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -317,15 +317,158 @@ class PaymentController extends Controller
 
     }
 
+
     public function shoketParticipantIndex($id){
 
         return view('participant_shoketpay');
     }
 
 
-    public function shoketParticipantPay(){
+    public function shoketParticipantPay(Request $request){
 
-     echo "good";
+      $request->validate([
+
+            'provider' => 'required',
+            'amount'=>'required',
+            'phone'=>'required',
+
+        ]);
+
+
+        $provider = $request->provider;
+        $amount = $request->amount;
+
+        if($amount >= 1000 ){
+
+        $phone = $request->phone;
+        $event_id = $request->event_id;
+        $event = Event::find($event_id);
+        $event_title = $event->event_title;
+
+
+        $user_id = session('loginId');
+        $user = User::find($user_id);
+        $user_name = $user->name;
+        $user_email = $user->email;
+
+        $chargeResponse = Shoket::makePaymentRequest([
+            "amount" => $amount,
+            "customer_name" => $user_name,
+            "email" =>  $user_email,
+            "number_used" => $phone,
+            "channel" =>  $provider
+        ]);
+
+        //  dd($chargeResponse)  ;
+
+
+       $data = $chargeResponse['data'];
+       $customer = $chargeResponse['customer'];
+
+
+
+   $referenceId = $chargeResponse['reference'];
+   $response = Shoket::verifyPaymentRequest($referenceId, [
+     "provider_name" => "Tigo",
+     "provider_code" => "Tigo pesa"
+     ]);
+      dd($response);
+
+
+       $amount = $data['amount'];
+       $channel = $data['channel'];
+       $reference_no =   $chargeResponse['reference'];
+       $number =  $data['number_used'];
+       $time = Carbon::now();
+
+
+     if ($data['payment_status'] == "Pending" ){
+
+        $payment = new Payment();
+        $payment->payment_time = Carbon::now();
+        $payment->amount = $data['amount'];
+        $payment->method = $data['channel'];
+        $payment->phone_number = $data['number_used'];
+        $payment->reference_no = $chargeResponse['reference'];
+        $payment-> event_id = $event_id;
+        $payment-> user_id =  $user_id;
+        $res = $payment->save();
+
+
+     if($res){
+
+         $amountpaid = Payment::all()->where('event_id',$event_id)->where('user_id',$user_id)->sum('amount');
+         $amounttotal = EventDetail::all()->where('event_id',$event_id)->pluck('price');
+
+         $price = $amounttotal[0];
+         $percent = ($amountpaid/ $price) * 100 ;
+
+
+      if($percent >= 100){
+
+        $link = url('ticket/'.$event_id);
+
+        $time = time();
+        $qr = md5($time);
+
+        $ticket = Ticket::all()->where('event_id', $event_id)->pluck('reference_no');
+
+        if($ticket == "[]")
+        {
+             $number = 0;
+        }else{
+            $number = $ticket[0] + 1 ;
+        }
+
+        $ticket = new Ticket();
+        $ticket->barcode_no = $qr;
+        $ticket->event_id = $event_id;
+        $ticket->user_id = $user_id;
+        $ticket->amount   =  $amounttotal[0];
+        $ticket->reference_no =  $chargeResponse['reference'];
+        $ticket->save();
+
+        $values = [
+            'title' => 'Dear '. $user_name.',',
+            'body1' => 'You have paid for '. $event_title.' event at '.$time.'.',
+            'body2' => 'Amount paid is Tsh '. $amount.' via '. $channel. ' phone number '.$number.' Payment reference number is '.$reference_no,
+            'body' => 'This is the ticket of '. $event_title.' event.',
+            'link' => $link,
+        ];
+
+         Mail::to($user_email)->send(new TicketMail($values));
+      }else{
+
+
+
+        $details = [
+            'title' => 'Dear '. $user_name.',',
+            'body1' => 'You have paid for '. $event_title.' event at '.$time.'.',
+            'body2' => 'Amount paid is Tsh '. $amount.' via '. $channel. ' phone number '.$number.' Payment reference number is '.$reference_no,
+
+        ];
+
+          Mail::to($user_email)->send(new PaymentMail($details));
+
+      }
+
+
+        return back()->with('success','Payment done successful');
+        // return response()->json(['success'=>'Payment Done Successful']);
+
+     }
+    }
+     else{
+        return back()->with('fail', 'Payment Failed');
+        // return response()->json(['success'=>'Payment Failed']);
+     }
+
+    }
+    else{
+        return back()->with('fail', 'Payment should not be below Tsh 1,000');
+    }
+
+
     }
 
 
